56235e0f4b1133bef57f7578510fe3082e79d927
luzardo linux <reudiluzar10@gmail.com>
2025-05-19 18:24:34 -0400

    mas optimizacion x2

--- codigo.js
+++ codigo.js
@@ -1,46 +1,36 @@
-const entrada_numero = document.querySelector(".form-control");
+const $ = (e) => document.querySelector(e);
+const input_num= $(".form-control");
+const boton_1 = $(`.btn1`);
+const boton_2 = $(`.btn2`);
+const boton_3 = $(`.btn3`);
+const boton_4 = $(`.btn4`);
+const boton_5 = $(`.btn5`);
+const boton_6 = $(`.btn6`);
+const boton_7 = $(`.btn7`);
+const boton_8 = $(`.btn8`);
+const boton_9 = $(`.btn9`);
+const boton_0 = $(`.btn0`);
 
+const boton_suma = $(`.btn-suma`);
+const boton_resta = $(`.btn-resta`);
+const boton_multiplicacion = $(`.btn-multiplicacion`);
+const boton_division = $(`.btn-division`);
+const boton_total = $(`.btntotal`);
 
-const boton_1 = document.querySelector(".btn1");
-const boton_2 = document.querySelector(".btn2");
-const boton_3 = document.querySelector(".btn3");
-const boton_4 = document.querySelector(".btn4");
-const boton_5 = document.querySelector(".btn5");
-const boton_6 = document.querySelector(".btn6");
-const boton_7 = document.querySelector(".btn7");
-const boton_8 = document.querySelector(".btn8");
-const boton_9 = document.querySelector(".btn9");
-const boton_0 = document.querySelector(".btn0");
+boton_1.addEventListener("click",()=>{input_num.value += "1";});
+boton_2.addEventListener("click",()=>{input_num.value += "2";});
+boton_3.addEventListener("click",()=>{input_num.value += "3";});
+boton_4.addEventListener("click",()=>{input_num.value += "4";});
+boton_5.addEventListener("click",()=>{input_num.value += "5";});
+boton_6.addEventListener("click",()=>{input_num.value += "6";});
+boton_7.addEventListener("click",()=>{input_num.value += "7";});
+boton_8.addEventListener("click",()=>{input_num.value += "8";});
+boton_9.addEventListener("click",()=>{input_num.value += "9";});
+boton_0.addEventListener("click",()=>{input_num.value += "0";});
 
+boton_suma.addEventListener("click",()=>{input_num.value += "+";});
+boton_resta.addEventListener("click",()=>{input_num.value += "-";});
+boton_multiplicacion.addEventListener("click",()=>{input_num.value += "*";});
+boton_division.addEventListener("click",()=>{input_num.value += "/";});
 
-
-const boton_suma = document.querySelector(".btn-suma");
-const boton_resta = document.querySelector(".btn-resta");
-const boton_multiplicacion = document.querySelector(".btn-multiplicacion");
-const boton_division = document.querySelector(".btn-division");
-const boton_total = document.querySelector(".btntotal");
-
-
-boton_1.addEventListener("click",()=>{entrada_numero.value += "1";});
-boton_2.addEventListener("click",()=>{entrada_numero.value += "2";});
-boton_3.addEventListener("click",()=>{entrada_numero.value += "3";});
-boton_4.addEventListener("click",()=>{entrada_numero.value += "4";});
-boton_5.addEventListener("click",()=>{entrada_numero.value += "5";});
-boton_6.addEventListener("click",()=>{entrada_numero.value += "6";});
-boton_7.addEventListener("click",()=>{entrada_numero.value += "7";});
-boton_8.addEventListener("click",()=>{entrada_numero.value += "8";});
-boton_9.addEventListener("click",()=>{entrada_numero.value += "9";});
-boton_0.addEventListener("click",()=>{entrada_numero.value += "0";});
-
-boton_suma.addEventListener("click",()=>{entrada_numero.value += "+";});
-boton_resta.addEventListener("click",()=>{entrada_numero.value += "-";});
-boton_multiplicacion.addEventListener("click",()=>{entrada_numero.value += "*";});
-boton_division.addEventListener("click",()=>{entrada_numero.value += "/";});
-
-
-boton_total.addEventListener("click",totalnumero);
-
-function totalnumero(){
-	const resultado = math.simplify(entrada_numero.value);
-	entrada_numero.value = resultado;
-}
+boton_total.addEventListener("click",()=>{input_num.value = math.simplify(input_num.value)});
